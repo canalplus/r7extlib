0d4c7aaf739caefb6520d9b6edd318022ab2046d
Damien Ragoucy <damien.ragoucy@af83.com>
2013-10-01 11:50:27 +0200

    Initial commit

---
+++ r7.js
@@ -0,0 +1,108 @@
+(function(global) {
+
+  var uid = 0;
+
+  var AVAILABLE_KEYS = {
+    'Up':       true,
+    'Down':     true,
+    'Right':    true,
+    'Left':     true,
+    'Enter':    true,
+    'Mute':     true,
+    'Zoom':     true,
+    'Back':     true,
+    'Exit':     true,
+    'Guide':    true,
+    'Menu':     true,
+    'Numeric0': true,
+    'Rewind':   true,
+    'Play':     true,
+    'Forward':  true,
+    'Stop':     true,
+    'Pause':    true,
+    'Rec':      true,
+    'TV':       true
+  };
+
+  grabedKeys = {};
+
+  function _extractKey(key) {
+    if (typeof key === 'string') { return key; }
+    if (Array.isArray(key)) { key = key[0]; }
+    if (typeof key === 'object') { return Object.keys[0]; }
+    return key;
+  }
+
+  function send(action, params) {
+    window.parent.postMessage({
+      id: uid,
+      action: action,
+      params: params
+    }, '*');
+    return uid++;
+  }
+
+  function rpc(method, params, callback, context) {
+    if (typeof params === 'function') {
+      context  = callback;
+      callback = params;
+      params   = null;
+    }
+    var id = send(method, params);
+    window.addEventListener('message', function getter(msg) {
+      if (msg.data.id === id) {
+        callback.call(context || null, msg.data.response);
+        window.removeEventListener('message', getter);
+      }
+    }, false);
+  }
+
+  function grabKey(key, callback, context) {
+    // Get the key string in case the infos is passed like ["KeyName"] or
+    // { Up: { after: 300, throttle: 50 } }
+    var keyString = _extractKey(key);
+    if (!AVAILABLE_KEYS[keyString]) { return; }
+    // G7 doesn't take keys as pure strings
+    send('addKeys', typeof key === 'string' ? [key] : key);
+    function keyPressHandler(msg) {
+      if (keyString === msg.data.key) { callback.call(context); }
+    }
+    // Remove handler if key is already grabbed
+    if (grabedKeys[keyString])
+      { window.removeEventListener('message', grabedKeys[keyString]); }
+    // Register freshly created handler
+    grabedKeys[keyString] = keyPressHandler;
+    window.addEventListener('message', keyPressHandler, false);
+  }
+
+  function releaseKey(key) {
+    // Get the key string in case the infos is passed like ["KeyName"] or
+    // { Up: { after: 300, throttle: 50 } }
+    var keyString = _extractKey(key);
+    if (!AVAILABLE_KEYS[keyString]) { return; }
+    if (grabedKeys[keyString]) {
+      send('removeKeys', typeof key === 'string' ? [key] : key);
+      window.removeEventListener('message', grabedKeys[keyString]);
+      delete grabedKeys[keyString];
+    }
+  }
+
+  function onReadyState(callback, context) {
+    window.addEventListener('load', function boot() {
+      send('ready');
+      callback.call(context);
+      window.removeEventListener('load', boot);
+    });
+  }
+
+  var r7 = {};
+
+  r7.send = send;
+  r7.rpc = rpc;
+  r7.grabKey = grabKey;
+  r7.releaseKey = releaseKey;
+  r7.onReadyState = onReadyState;
+
+  global.r7 = r7;
+
+})(this);
