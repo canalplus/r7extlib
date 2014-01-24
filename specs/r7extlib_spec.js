d3e5e818a061c21872f7a9224e2f7f5510888d5e
Damien Ragoucy <damien.ragoucy@af83.com>
2014-01-24 11:23:12 +0100

    Added "addStreamListener" to key existence test
    And reformatted a bit

--- specs/r7extlib_spec.js
+++ specs/r7extlib_spec.js
@@ -1,14 +1,22 @@
 var expect = chai.expect;
 
 describe('r7extlib', function() {
+
   it('exists', function() {
     expect(R7).to.exist;
   });
 
-  function keyTest(key) {
-    it('has a ' + key + ' method', function() {
-      expect(R7[key]).to.be.a('function');
-    });
-  }
-  ['send', 'rpc', 'grabKey', 'releaseKey', 'onReadyState'].forEach(keyTest);
+  describe('has method:', function() {
+    function keyTest(key) {
+      it(key, function() {
+        expect(R7[key]).to.be.a('function');
+      });
+    }
+    var methodNames = [
+      'send', 'rpc', 'grabKey', 'releaseKey', 'onReadyState',
+      'addStreamListener'
+    ];
+    methodNames.forEach(keyTest);
+  });
+
 });
